32aa1556a18e950a446773fdc8d5d47da723e08c
pongo-pygmaeus <maxwellmathews@gmail.com>
2017-05-10 10:03:11 -0700

    Adds schema, comments about incorrectness of belongs_to through

--- app/models/barnyard_animal.rb
+++ app/models/barnyard_animal.rb
@@ -1,6 +1,6 @@
 class BarnyardAnimal < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :farm
-  has_one :farmer, through: :farm
+  has_one :farmer, through: :farm #remember, we can't do belongs_to through...
   belongs_to :animal_type
 end
